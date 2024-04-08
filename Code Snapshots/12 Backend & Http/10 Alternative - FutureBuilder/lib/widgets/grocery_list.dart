b4000cc343759f8e1f51be44072844165f5e7676
Chimuka Moonde <chimukamoonde@gmail.com>
2024-04-08 23:56:07 +0200

    minor adjustments

--- Code Snapshots/12 Backend & Http/10 Alternative - FutureBuilder/lib/widgets/grocery_list.dart
+++ Code Snapshots/12 Backend & Http/10 Alternative - FutureBuilder/lib/widgets/grocery_list.dart
@@ -18,6 +18,9 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
 
   List<GroceryItem> _groceryItems = [];
+
+  //tells Dart that we have no initial value for loadedItems, but we will have a value
+  // before it's being used the first time.
   late Future<List<GroceryItem>> _loadedItems;
   String? _error;
 
@@ -28,14 +31,30 @@ class _GroceryListState extends State<GroceryList> {
   }
 
 
+  //Instead of returning void here, we could say
+  // that this function should return a list of grocery items
+  //since we're using async here, this will automatically return a future
+  //This async keyword basically ensures that the function that's decorated with it
+  // will always produce a future, automatically.
+  //Therefore, the correct type annotation for this function here, for this method,
+  // is that we'll get back a future which will eventually resolve to a list of grocery items,
+  //So now loadItems should be a method that returns a future,
+  // which it will do automatically because of async, as you learned,
+  // which will then in turn resolve to a list of grocery items.
   Future<List<GroceryItem>> _loadItems() async {
 
     final url = Uri.https('flutter-prep-default-rtdb.firebaseio.com', 'shopping-list.json');
 
+    //I will get rid of this overall try-catch block because with FutureBuilder, as you will see,
+    // we can handle errors in a different way, in a way that requires less code from our side,
+    // which can be quite convenient.
     final response = await http.get(url);
 
     //if status is greater than 400, there is a problem
     if (response.statusCode >= 400) {
+      //if we throw an exception or if any other code in there throws an exception.
+      // In that case, as mentioned, the future will be rejected
+      // and that would lead to this hasError property
       throw Exception('Failed to fetch grocery items. Please try again later.');
     }
 
@@ -67,6 +86,7 @@ class _GroceryListState extends State<GroceryList> {
       );
     }
 
+    // get rid of  setState action here
     return loadedItems;
   }
 
@@ -130,22 +150,37 @@ class _GroceryListState extends State<GroceryList> {
         future: _loadedItems,
         builder: (context, snapshot) {
 
+          // a snapshot, which in the end gives you access to the current state
+          // of the future and the data that might have been produced.
+
+          //use this snapshot to evaluate the current state of that future.
+
+          //the initial state when we sent the request and we're still waiting for the response.
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           }
 
           //not waiting ,now we have a result with data, error or no data
+
+          //use this snapshot object again to check whether we might have an error
           if (snapshot.hasError) {
             return Center(
               child: Text(snapshot.error.toString(),),
             );
           }
 
+          //check if snapshot data, which is the data we will have in the end,
+          // if that is not empty.
           if (snapshot.data!.isEmpty) {
             return const Center(child: Text('No items added yet.'));
           }
 
+          // if we also make it past this if check, we know that data is not empty,
+          // that it is a list of grocery items,
           return ListView.builder(
+            //this builder function in the end exposes the state and the data produced by that future
+            // through this snapshot object.
+
             itemCount: snapshot.data!.length,
             itemBuilder: (ctx, index) => Dismissible(
               onDismissed: (direction) {
