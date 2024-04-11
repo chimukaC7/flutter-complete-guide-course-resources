015aa3edb790eb16fae46ca0218f83905627197a
Chimuka Moonde <chimukamoonde@gmail.com>
2024-04-11 09:10:15 +0200

    minor adjustments

--- Code Snapshots/14 Chat App/11 Finished/lib/widgets/chat_messages.dart
+++ Code Snapshots/14 Chat App/11 Finished/lib/widgets/chat_messages.dart
@@ -11,13 +11,17 @@ class ChatMessages extends StatelessWidget {
     final authenticatedUser = FirebaseAuth.instance.currentUser!;
 
     //we can update the UI whenever a new document is added to this collection.
+    //whenever we add a new message like this should work you see that's added here automatically.
+    // We don't have to manage any state manually
+    // we don't have to do anything like that
+    // We just submit the data,then thanks to this stream here, so to this listener which we in the end have here
+
+    // it's all updated automatically thereafter.
     return StreamBuilder(
       stream: FirebaseFirestore.instance
           .collection('chat')
-          .orderBy(
-            'createdAt',
-            descending: true,
-          )
+        //make sure that my latest message, is at the bottom of that list though.
+          .orderBy('createdAt',descending: true,)
           .snapshots(),
       builder: (ctx, chatSnapshots) {
         // connection state waiting, we know that we're still waiting to receive some data.
@@ -50,25 +54,31 @@ class ChatMessages extends StatelessWidget {
             left: 13,
             right: 13,
           ),
+          //the entire list is reversed
+          // And instead of going from top to bottom, it now goes from bottom to top.
           reverse: true,
           itemCount: loadedMessages.length,
           itemBuilder: (ctx, index) {
             final chatMessage = loadedMessages[index].data();
             final nextChatMessage = index + 1 < loadedMessages.length
+            //if there is a next message, we will get hold of it by using loadedMessages
+            // for index plus one.
                 ? loadedMessages[index + 1].data()
                 : null;
 
             final currentMessageUserId = chatMessage['userId'];
-            final nextMessageUserId =
-                nextChatMessage != null ? nextChatMessage['userId'] : null;
+            final nextMessageUserId = nextChatMessage != null ? nextChatMessage['userId'] : null;
             final nextUserIsSame = nextMessageUserId == currentMessageUserId;
 
             if (nextUserIsSame) {
+              //if the user is the same as the current user, we want to return MessageBubble.next
+              // because we have an ongoing sequence of messages from the same user.
               return MessageBubble.next(
                 message: chatMessage['text'],
                 isMe: authenticatedUser.uid == currentMessageUserId,
               );
             } else {
+              // the first time that we're outputting a message for the given user.
               return MessageBubble.first(
                 userImage: chatMessage['userImage'],
                 username: chatMessage['username'],
